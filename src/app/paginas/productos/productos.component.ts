8fa8035b5652be8ef25b59e64d98390f86db9301
ElGrandotex <jose.verdezoto.moran@gmail.com>
2024-03-21 19:19:59 -0500

    Obtencion de data

--- src/app/paginas/productos/productos.component.ts
+++ src/app/paginas/productos/productos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ProductoService } from '../../servicios/producto.service';
 
 @Component({
   selector: 'app-productos',
@@ -9,14 +10,50 @@ import { Component } from '@angular/core';
 })
 export class ProductosComponent {
 
-  constructor(){}
+  constructor(private productoSrv: ProductoService) { }
 
-  desplazar(id: string): void{
+  ngOnInit() {
+    this.productoSrv.obtenerDataHamburguesas()
+    this.productoSrv.obtenerDataKebab()
+    this.productoSrv.obtenerDataLasagnas()
+    this.productoSrv.obtenerDataOtros()
+    this.productoSrv.obtenerDataPaninis()
+    this.productoSrv.obtenerDataPizzas()
+    this.productoSrv.obtenerDataPorciones()
+    this.productoSrv.obtenerDataTacos()
+  }
+
+  desplazar(id: string): void {
     const targetElement = document.getElementById(id);
     targetElement?.scrollIntoView({
       behavior: "smooth",
     });
     targetElement?.scrollBy(0, -targetElement.getBoundingClientRect().top)
+  }
+
+  get hamburguesas(){
+    return this.productoSrv.hamburguesas;
+  }
+  get kebabs(){
+    return this.productoSrv.kebab;
+  }
+  get lasagnas(){
+    return this.productoSrv.lasagnas;
+  }
+  get otros(){
+    return this.productoSrv.otros;
+  }
+  get paninis(){
+    return this.productoSrv.paninis;
+  }
+  get pizzas(){
+    return this.productoSrv.pizzas;
+  }
+  get porciones(){
+    return this.productoSrv.porciones;
+  }
+  get tacos(){
+    return this.productoSrv.tacos;
   }
 
 }
